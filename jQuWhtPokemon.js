49090f148a263ce2823d6f1332c284259928e0e4
Ali Bayati <bayati.ali@icloud.com>
2018-04-26 13:34:35 -0500

    first Commit

---
+++ jQuWhtPokemon.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    
+
+
+
+    for(var i=1;i<151;i++){
+    $('#dispPok').append('<img alt='+i+' src="https://pokeapi.co/media/img/'+i+'.png" />');
+    }
+    $('img').click(function(){
+        var ni=$(this).attr("alt")
+        $.get("https://pokeapi.co/api/v2/pokemon/"+ni+"/",function(res){
+        console.log(res);
+        $('#pokDetl').append("<h1>"+res.name+"</h1>");
+        $('#pokDetl').append('<img  src="https://pokeapi.co/media/img/'+ni+'.png" />');
+        var html_str ="";
+        html_str +="<h2>Types</h2><ul>";
+        for(var j=0;j<res.types.length;j++){
+            
+            html_str+="<li>"+res.types[j].type.name;+"</li>";
+        }
+        html_str+="</ul>";
+       
+        html_str+="<h2>Height</h2><p>"+res.height+"</p>"+"<h2>Weight</h2>"+res.weight;
+        $('#pokDetl').append(html_str);
+
+    },'json');
+    })
+});
